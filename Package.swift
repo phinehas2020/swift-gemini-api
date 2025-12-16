4ee6297f5ba3a77441aa1c50e7804fb245e887f4
phinehas2020 <phinehas@example.com>
2025-12-16 13:04:25 -0600

    Fix Package.swift: Use Swift tools version 5.9 and lower macOS requirement
    Swift tools version 6.2 is not yet released and causes fatalError during
    package resolution. Changed to 5.9 which is compatible with Xcode 16.x.
    Also lowered macOS requirement from v15 to v12 for broader compatibility.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "swift-gemini-api",
 	platforms: [
 		.iOS(.v14),
-		.macOS(.v15)
+		.macOS(.v12)
 	],
     products: [
         .library(
